2e3d68d292192137a0fdf849fd517ab766190ee1
AbbosIsmailov <abbosismailov@gmail.com>
2023-11-22 10:29:59 +0500

    finished update and delete

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import HomePage, add_news_view, PostDetailView
+from .views import HomePage, add_news_view, PostDetailView, NewsDeleteView, NewsUpdateView
 
 urlpatterns = [
     path('', HomePage.as_view(), name="home"),
     path('add-new/', add_news_view, name="add_new"),
 
     path('post-details/<int:id>/', PostDetailView.as_view(), name='post_details'), # class-based view
-    # path('post-details/<int:id>/', post_details, name='post-details'), # function-based view 
+    path('<int:pk>/delete/', NewsDeleteView.as_view(), name='new_delete'),
+    path('<int:pk>/update/', NewsUpdateView.as_view(), name='new_update'),
 ]
